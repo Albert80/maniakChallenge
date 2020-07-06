daddb60727701994b52aede16662b226bf4ee1fc
César Alberto Trejo <cesaratj27@gmail.com>
2020-07-06 12:21:29 -0500

    Se crean las vistas: Layout, Bellotero y Testimonial

---
+++ src/config/urls.js
@@ -0,0 +1,5 @@
+export default {
+  menu: "https://raw.githubusercontent.com/Bernabe-Felix/Bellotero/master/app.json",
+  page1: "https://raw.githubusercontent.com/Bernabe-Felix/Bellotero/master/page1.json",
+  page2: "https://raw.githubusercontent.com/Bernabe-Felix/Bellotero/master/page2.json"
+};
